71f69f6e74e175369384214489e218a83706c734
Sadaqatullah <sadaqatullah.noonari@gmail.com>
2020-01-31 10:53:19 +0500

    added sonar to measure quality of my code

--- Station/home/median.js
+++ Station/home/median.js
@@ -11,22 +11,19 @@ function median(data) {
     let data_length = data.length;
 
     console.log(data);
+    console.log(data_length);
 
     if ((data_length % 2) === 1) {
+        if (0 in data){
+            return data[Math.floor(data_length/2)+1]
+        }
         return data[Math.floor(data_length / 2)]
     } else {
-        console.log((
-            sorted_data[Math.floor(data_length / 2)] +
-            sorted_data[Math.floor(data_length / 2) - 1]
-        ) / 2)
-
         return (
             sorted_data[Math.floor(data_length / 2)] +
             sorted_data[Math.floor(data_length / 2) - 1]
         ) / 2
     }
-
-    return data;
 }
 
 
@@ -35,5 +32,6 @@ if (!global.is_checking) {
     assert.equal(median([3, 1, 2, 5, 3]), 3, "2nd example");
     assert.equal(median([1, 300, 2, 200, 1]), 2, "3rd example");
     assert.equal(median([3, 6, 20, 99, 10, 15]), 12.5, "4th example");
+    assert.equal(median([10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]), 5, "4th example");
     console.log("Coding complete? Click 'Check' to review your tests and earn cool rewards!");
 }
